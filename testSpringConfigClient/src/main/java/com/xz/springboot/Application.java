ff124f0f6cac1cd1b55fc9280abc15c821bef74d
xuzheng <xuzheng@tusvn.com>
2018-08-06 14:58:04 +0800

    添加测试工程

--- testSpringConfigClient/src/main/java/com/xz/springboot/Application.java
+++ testSpringConfigClient/src/main/java/com/xz/springboot/Application.java
@@ -1,14 +1,30 @@
 package com.xz.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaServer
+@RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Value("${foo}")
+	private String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi() {
+		return foo;
 	}
 }
--- testSpringConfigEurekaServer/src/main/java/com/xz/springboot/Application.java
+++ testSpringConfigEurekaServer/src/main/java/com/xz/springboot/Application.java
@@ -2,12 +2,10 @@ package com.xz.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
-@EnableEurekaClient
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
